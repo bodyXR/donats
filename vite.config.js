0812766d191b74bdc32242323ecbdabbd5fb7047
Khaled Basta <khaled.basta@protonmail.com>
2023-08-29 16:38:23 +0300

    Adjust config. file

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: react({
     // Use React plugin in all *.jsx and *.tsx files
-    include: "**/*.{jsx,tsx}",
+    include: [/\.jsx$/],
   }),
 });
